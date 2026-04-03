35c8474b63db1f7722e0f8c3738916b5f76e4a23
nuthiokhaw99 <confixxx555@gmail.com>
2026-04-03 23:53:49 +0700

    Create config.example.js

---
+++ js/config.example.js
@@ -0,0 +1,5 @@
+
+const CONFIG = {
+  SUPABASE_URL:  'https://wslevsdsbcqjndskwyhz.supabase.co',
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzbGV2c2RzYmNxam5kc2t3eWh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NzIzODMsImV4cCI6MjA5MDA0ODM4M30.oqU-hjSy0ME_BBJq5zeyvUgjOC4g4sQHZmxbA0Tp9B8'
+};
