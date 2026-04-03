267c401c51ba1e5b3b45db46b61622eeaad95934
nuthiokhaw99 <confixxx555@gmail.com>
2026-04-04 00:10:13 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// config.js — ห้าม commit ไฟล์นี้เด็ดขาด
+// ไฟล์นี้อยู่ใน .gitignore แล้ว
+const CONFIG = {
+  SUPABASE_URL:  'https://wslevsdsbcqjndskwyhz.supabase.co',
+  SUPABASE_ANON: 'sb_publishable_KV4dA3OcnSzWMZjdz3sTvA_cigdtjCj'
+};
